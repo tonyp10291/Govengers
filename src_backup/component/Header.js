9648ab4e191f032f2f8cc3c7bf06eff9316c43db
BlueRabbit-kim <whddbs1594@gmail.com>
2025-07-29 10:55:47 +0900

    오류 수정

--- src_backup/component/Header.js
+++ src_backup/component/Header.js
@@ -1,11 +1,13 @@
-import HeaderTop from './components/TopHeader';
+import TopHeader from './TopHeader';
 
-function App() {
+function Header() {
   return (
     <>
-      <HeaderTop />
-      <header className="home-header"> ... </header>
-      <main className="home-main"> ... </main>
+      <TopHeader />
+      <header className="home-header"> 홈헤더 </header>
+      <main className="home-main"> 홈헤더 </main>
     </>
   );
 }
+
+export default Header;
